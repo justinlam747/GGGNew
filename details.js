dacd2da309303d73ce66ddee3eb9bf1aa30bff78
justin <113154738+justinlam6@users.noreply.github.com>
2025-04-06 00:59:32 -0400

    back to og

--- details.js
+++ details.js
@@ -2,9 +2,6 @@ export const details = [
   { id: 4535074190, name: "Consume" },
   { id: 7230509689, name: "Find the Seals" },
   { id: 7335437420, name: "Find the Hamsters" },
-  { id: 3359215813, name: "Doglum" },
-  { id: 2955322961, name: "Shrimp Game" },
-  { id: 7253578148, name: "Grow your Poop" },
 ];
 
 export const gameDetails = [{}, {}];
