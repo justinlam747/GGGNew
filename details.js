0da5989959815473521e1cf0688bf5e9c9794524
justin <113154738+justinlam6@users.noreply.github.com>
2025-04-25 13:52:32 -0400

    Update details.js

--- details.js
+++ details.js
@@ -2,8 +2,7 @@ export const details = [
   { id: 4535074190, name: "Consume", show: true },
   { id: 7230509689, name: "Find the Seals", show: true },
   { id: 7335437420, name: "Find the Hamsters", show: true },
-  { id: 7390254166, name: "Find the Floppas", show: false },
-  { id: 7436755782, name: "Grow A Garden", show: false },
+  { id: 7390254166, name: "Find the Floppas", show: true},
 ];
 
 export const gameDetails = [{}, {}];
