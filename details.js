610717e856125e5e1c2ae7e8e752248380740948
Smlik1 <smlikstudiosofficial@gmail.com>
2025-09-14 12:30:30 -0400

    Update details.js

--- details.js
+++ details.js
@@ -3,9 +3,10 @@ export const details = [
   { id: 7230509689, name: "Find the Seals", show: true },
   { id: 7335437420, name: "Find the Hamsters", show: true },
   { id: 7390254166, name: "Find the Floppas", show: true},
+  { id: 7739421854, name: "Find The Dogs", show: true},
+  { id: 7975097812, name: "Find The Labubu", show: true},
   { id: 7584759903, name: "Tung Tung", show: true},
-  { id: 7813968568, name: "Tung Tung 1", show: true},
-  { id: 7739421854, name: "Tung Tung 2", show: true},
+  { id: 7813968568, name: "Escape Gubby", show: true},
   { id: 8591811455, name: "Escape Green Bean", show: true},
  
 ];
@@ -15,4 +16,5 @@ export const groupDetails = [
   { id: 17206753 },
   { id: 33725092 },
   { id: 35586387 },
+  { id: 35895209 },
 ];
